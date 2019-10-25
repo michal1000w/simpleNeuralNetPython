a596bd470122808522ffff84daee9f9f8ba4f0a8
michal1000w <michal_wieczorek@hotmail.com>
2019-10-25 22:26:49 +0200

    IRIS database test

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ if __name__ == '__main__':
 
     net = NeuNet.NeuNet()
 
-    im = Import.Import("INPUT\Training_Data\data3.txt")
+    im = Import.Import("INPUT\Training_Data\iris.txt")
 
     net.input(im.get_input())
     net.output(im.get_output())
     net.labels(im.get_labels())
 
-    net.iterations(10000)
+    net.iterations(30000)
     #net.seed(4) #optional
 
     #experimental
     net.go_experimental(experimental)
-    net.set_threads(0)
+    net.set_threads(2)
     #net.force_threads(True)  #to set up more threads than CPU cores
 
     net.Setup()
-    net.set_name("my_network_2")
+    net.set_name("Iris")
 
     #load network from file
     '''nim = Import.NetImport("OUTPUT\Saved_Networks\my_network_1.txt")
@@ -35,13 +35,13 @@ if __name__ == '__main__':
 
     if (experimental):  
         net.Train()
-        net.Think("[1,13,4]")
-        net.Think("[5,3,2]")
+        net.Think("[5.5,3.6,1.4,0.1]")
+        net.Think("[6.7,2.9,4.4,1.2]")
     else:   
         try:
             net.Train()
 
-            net.Think("[1,13,4]")
-            net.Think("[5,3,2]")
+            net.Think("[5.5,3.6,1.4,0.1]")
+            net.Think("[6.7,2.9,4.4,1.2]")
         except:
             print("Training Failed (Maybe too much input data -> try multithreaded workflow)")
